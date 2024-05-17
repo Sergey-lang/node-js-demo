fb3193570884fcec62bf4ecc08690c87beb08539
Siarhei <siarhei.kukharonak@startmatter.com>
2024-05-17 15:25:33 +0300

    add chalk, dedent for showing log

---
+++ services/log.service.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import dedent from 'dedent-js';
+
+const log = console.log;
+const printError = (error) => {
+    log(chalk.bgRed(' ERROR ', error));
+}
+
+const printSuccess = (message) => {
+    log(chalk.bgGreen(' SUCCESS ', message));
+}
+
+const printHelp = () => {
+    log(dedent(`${chalk.bgCyan(' HELP ')}
+    Without options - select city
+    -s [CITY] - for select city
+    -h [CITY] - for showing help
+    -t [API_KEY] - for saving token
+    `));
+}
+
+export { printSuccess, printError, printHelp }
